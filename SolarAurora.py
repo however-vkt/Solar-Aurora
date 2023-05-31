e59c73547f3ad9e5d2d24ba2ccd34d6b454654ed
however-vkt <however.viktor@gmail.com>
2023-05-31 15:33:08 +0300

    added: button connected with pages
    need:
    connection with functions

--- SolarAurora.py
+++ SolarAurora.py
@@ -16,34 +16,45 @@ class MainWindow(QMainWindow):
         icon.addPixmap(QPixmap('pic/x.png'))
         self.closeNotificationBtn.setIcon(icon)
         self.closeNotificationBtn.setIconSize(QSize(32, 32))
+        #
 
         icon.addPixmap(QPixmap('pic/menu.png'))
         self.MenuButton.setIcon(icon)
         self.MenuButton.setIconSize(QSize(32, 32))
+        #
 
-        icon.addPixmap(QPixmap('pic/flares.png'))
-        self.FlaresButton.setIcon(icon)
-        self.FlaresButton.setIconSize(QSize(32, 32))
-
+        # Кнопка - Sun
         icon.addPixmap(QPixmap('pic/sun.png'))
         self.SolarButton.setIcon(icon)
         self.SolarButton.setIconSize(QSize(32, 32))
+        self.SolarButton.clicked.connect(lambda: self.mainPages.setCurrentWidget(self.page_solar))
 
+        # Кнопка - Weather
         icon.addPixmap(QPixmap('pic/weather.png'))
         self.WeatherButton.setIcon(icon)
         self.WeatherButton.setIconSize(QSize(32, 32))
+        self.WeatherButton.clicked.connect(lambda: self.mainPages.setCurrentWidget(self.page_weather))
+
+        # Кнопка - Flares
+        icon.addPixmap(QPixmap('pic/flares.png'))
+        self.FlaresButton.setIcon(icon)
+        self.FlaresButton.setIconSize(QSize(32, 32))
+        self.FlaresButton.clicked.connect(lambda: self.mainPages.setCurrentWidget(self.page_flares))
 
         icon.addPixmap(QPixmap('pic/help.png'))
         self.helpBtn.setIcon(icon)
         self.helpBtn.setIconSize(QSize(32, 32))
+        self.helpBtn.clicked.connect(lambda: self.mainPages.setCurrentWidget(self.page_help))
 
         icon.addPixmap(QPixmap('pic/info.png'))
         self.infoBtn.setIcon(icon)
         self.infoBtn.setIconSize(QSize(32, 32))
+        self.infoBtn.clicked.connect(lambda: self.mainPages.setCurrentWidget(self.page_info))
 
         icon.addPixmap(QPixmap('pic/settings.png'))
         self.settingsBtn.setIcon(icon)
         self.settingsBtn.setIconSize(QSize(32, 32))
+        self.settingsBtn.clicked.connect(lambda: self.mainPages.setCurrentWidget(self.page_settings))
 
         self.show()
         # closeNotificationBtn
